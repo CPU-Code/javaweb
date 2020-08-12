88e411888fde91a2a46023da662aa8caead229e6
cpucode <923992029@qq.com>
2020-08-12 16:55:39 +0800

    js_math__计算

--- README.md
+++ README.md
@@ -6,20 +6,20 @@ HTML 语言 , Javascript , AJAX , CGI 编程
 
 ----------------
 
-- [x] [first_html_第一个网页](first_html)
-- [x] [meta_元信息](meta)
-- [x] [char_文字标签](char)
-- [x] [head_style_标题标签](head_style)
-- [x] [a_href_超链接标签](a_href)
-- [x] [table_表格标签](table)
-- [x] [form_表单](form)
-- [x] [js_method_网页使用js脚本方式](js_method)
-- [x] [js_window_浏览器对象](js_window)
-- [x] [js_document_文本对象](js_document)
-- [x] [js_date_Date时间](js_date)
-- [x] [time_setTimeout_time时间动态变化](time_setTimeout)
-- [x] [time_by_setinterval_time时间动态变化](time_by_setinterval)
-
+- [x] [first_html__第一个网页](first_html)
+- [x] [meta__元信息](meta)
+- [x] [char__文字标签](char)
+- [x] [head_style__标题标签](head_style)
+- [x] [a_href__超链接标签](a_href)
+- [x] [table__表格标签](table)
+- [x] [form__表单](form)
+- [x] [js_method__网页使用js脚本方式](js_method)
+- [x] [js_window__浏览器对象](js_window)
+- [x] [js_document__文本对象](js_document)
+- [x] [js_date__Date时间](js_date)
+- [x] [time_setTimeout__time时间动态变化](time_setTimeout)
+- [x] [time_by_setinterval__time时间动态变化](time_by_setinterval)
+- [x] [js_math__计算](js_math)
 
 
 ----------------
---
+++ js_math/js_math.js
@@ -0,0 +1,44 @@
+/*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-08-12 16:31:36
+ * @LastEditTime: 2020-08-12 16:55:03
+ * @FilePath: \web\js_math\js_math.js
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+function fun(deal_num)
+{
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    if(isNaN(num1) || isNaN(num2))
+    {
+        alert("输入数");
+
+        document.getElementById("num1").value="";
+
+        return;
+    }
+
+    var dest;//存储处理结果
+    switch(deal_num)
+    {
+        case 1:
+            alert("求较大值");
+            alert(Number(num1));
+            alert(Number(num2));
+            // 求最大值
+            dest = Math.max(Number(num1), Number(num2));
+            break;
+
+        case 2:
+            // 求和
+            dest = Number(num1) + Number(num2);
+            break;
+    }
+
+    document.getElementById("result").value = dest;//显示结果
+}
