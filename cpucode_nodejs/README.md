3854b47c3acc1a59874e773cdee909e98db57992
cpuCode <923992029@qq.com>
2021-03-22 10:26:30 +0800

    function__函数调用

--- cpucode_nodejs/README.md
+++ cpucode_nodejs/README.md
@@ -2,7 +2,7 @@
 # [cpucode_nodejs](../README.md)
 
 - [x] [demo__加法控制台输出](demo/demo.js)
-
+- [x] [function__函数调用](demo/function.js)
 
 
 -----------------
---
+++ cpucode_nodejs/demo/function.js
@@ -0,0 +1,15 @@
+/**
+ * @author : cpucode
+ * @date : 2021/3/22
+ * @time : 10:24
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+
+var c = add(100, 20);
+
+console.log(c);
+
+function add(a, b){
+    return a + b;
+}
