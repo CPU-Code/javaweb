8cff773385498241f72e4b867a9307780aed179d
cpuCode <923992029@qq.com>
2021-03-22 10:31:18 +0800

    require__模块

---
+++ cpucode_nodejs/demo/module/module.js
@@ -0,0 +1,11 @@
+/**
+ * @author : cpucode
+ * @date : 2021/3/22
+ * @time : 10:29
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+
+exports.add = function (a, b){
+    return a + b;
+}
---
+++ cpucode_nodejs/demo/module/require.js
@@ -0,0 +1,11 @@
+/**
+ * @author : cpucode
+ * @date : 2021/3/22
+ * @time : 10:29
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+
+var module = require('./module');
+
+console.log(module.add(199, 100));
